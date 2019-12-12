c5bc1262ce17d9936334b62f0f864a0c6a4610ad
banketree <banketree@qq.com>
2019-12-12 17:37:48 +0800

    引入BlocCanary 卡顿检测

--- performance_testing/src/main/java/com/example/performance_testing/config/ApplicationLifeCyclesImpl.kt
+++ performance_testing/src/main/java/com/example/performance_testing/config/ApplicationLifeCyclesImpl.kt
@@ -2,12 +2,12 @@ package com.example.performance_testing.config
 
 import android.app.Application
 import android.content.Context
-import com.alibaba.android.arouter.launcher.ARouter
-import com.example.base_fun.BuildConfig
 import com.example.base_lib.injection.core.AppLifeCycles
 import leakcanary.AppWatcher
 
 import timber.log.Timber
+import com.example.performance_testing.AppBlockCanaryContext
+import com.github.moduth.blockcanary.BlockCanary
 
 
 internal class ApplicationLifeCyclesImpl : AppLifeCycles {
@@ -15,7 +15,9 @@ internal class ApplicationLifeCyclesImpl : AppLifeCycles {
     }
 
     override fun onCreate(application: Application) {
+        Timber.i("${application.javaClass.simpleName} onCreate 初始化 + 泄漏检测 + 卡顿检测")
         AppWatcher.config = AppWatcher.config.copy()
+        BlockCanary.install(application, AppBlockCanaryContext()).start()
     }
 
     override fun onTerminate(application: Application) {
